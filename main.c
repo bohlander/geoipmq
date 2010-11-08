1aae92c9e29dc5e0251f6f0f1aa88ecef232cd23
Mike Bohlander <bohlander@gmail.com>
2010-11-07 16:22:04 -0800

    handle return codes better

--- main.c
+++ main.c
@@ -1,5 +1,5 @@
 /*
-Copyright (c) <2010> Mike BOhlander
+Copyright (c) <2010> Mike Bohlander
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
@@ -24,9 +24,11 @@ THE SOFTWARE.
 #include <GeoIP.h>
 #include <GeoIPCity.h>
 
+#include <assert.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <stdio.h>
+#include <stdarg.h>
 #include <stdlib.h>
 #include <fcntl.h>
 #include <errno.h>
@@ -36,14 +38,23 @@ THE SOFTWARE.
 
 #define MAX_BUFFER 250
 
-GeoIP* init_geoip(const char* filename) {
+void log_error(const char* error, ...) {
+  va_list args;
+  va_start(args,error);
+  vfprintf(stderr, error,args);
+  va_end(args);
+}
+
+GeoIP* init_geoip(const char* filename)
+{
   return GeoIP_open(filename, GEOIP_STANDARD);  
 }
 
-int resolve_ip(GeoIP* geoip, const char* ipaddr, char* out, int max_out) {
+int resolve_ip(GeoIP* geoip, const char* ipaddr, char* out, int max_out)
+{
   GeoIPRecord *r = GeoIP_record_by_addr(geoip, ipaddr);
   int chars_written = 0;
-  if (NULL == r) {
+  if (r == NULL) {
     chars_written = snprintf(out, max_out, "NOT_FOUND");
   }
   else {
@@ -55,46 +66,58 @@ int resolve_ip(GeoIP* geoip, const char* ipaddr, char* out, int max_out) {
   return chars_written;
 }
 
-
-
 int main (int argc, char **argv) {
   if (argc != 3) {
-    fprintf(stderr, "Usage: geoip_server geoipdb port");
-    return 0;
+    log_error("Usage: geoip_server geoipdb port\n");
+    return 1;
   }
 
   GeoIP* gi = init_geoip(argv[1]);
   if (gi == NULL) {
-    fprintf(stderr, "Could not open geoip database %s", argv[1]);
-    return 0;
+    log_error("Could not open geoip database %s\n", argv[1]);
+    return 1;
   }
   void *context = zmq_init(1);
+  if (context == NULL) {
+    log_error("Could not initialize ZMQ\n");
+    return 1;
+  }
 
   //  Socket to talk to clients
   void *responder = zmq_socket(context, ZMQ_REP);
+  if (responder == NULL) {
+    log_error("Could not initialize ZMQ responder\n");
+    return 1;
+  }
 
   // Check port
   char port[25];
   if (strlen(argv[2]) > 6 || atoi(argv[2]) == 0) {
-    fprintf(stderr, "Invalid port %s", argv[2]);
+    log_error("Invalid port %s", argv[2]);
+    return 1;
   }
   
+  int rc;
   sprintf(port, "tcp://*:%s", argv[2]);
-  zmq_bind (responder, port);
+  rc = zmq_bind (responder, port);
+  assert(rc == 0);
 
   while (1) {
     char input[MAX_BUFFER];
     //  Wait for next request from client
     zmq_msg_t request;
-    zmq_msg_init(&request);
-    zmq_recv(responder, &request, 0);
+    rc = zmq_msg_init(&request);    
+    assert(rc == 0);
+    rc = zmq_recv(responder, &request, 0);
+    assert(rc == 0);
 
-    // Copy and null terminate incoming message
     int size = zmq_msg_size(&request);
     if (size + 1 >= MAX_BUFFER ) {
+      // treat this as an error, this message is too large to be a valid geoip request.
       goto bad_message;
     }
 
+    // Copy and null terminate incoming message
     char* req_data = (char*) zmq_msg_data(&request);
     strncpy(input, req_data, size);
     input[size] = 0;
@@ -106,9 +129,11 @@ int main (int argc, char **argv) {
       int bytes = resolve_ip(gi, input + 6, output, MAX_BUFFER);
       
       zmq_msg_t reply;
-      zmq_msg_init_size(&reply, bytes);
+      rc = zmq_msg_init_size(&reply, bytes);
+      assert(rc == 0);
       memcpy((void*)zmq_msg_data(&reply), output, bytes);
-      zmq_send(responder, &reply, 0);
+      rc = zmq_send(responder, &reply, 0);
+      assert(rc == 0);
       zmq_msg_close(&reply);
 
     }
@@ -120,13 +145,16 @@ int main (int argc, char **argv) {
 
   bad_message:
     // Bail, invalid input.
-    zmq_msg_close (&request);
+    rc = zmq_msg_close (&request);
+    assert(rc == 0);
     zmq_msg_t reply;
-    zmq_msg_init_size (&reply, 6);
-    memcpy ((void *) zmq_msg_data (&reply), "ERROR", 5);
-    zmq_send (responder, &reply, 0);
-    zmq_msg_close (&reply);      
-
+    rc = zmq_msg_init_size(&reply, 6);
+    assert(rc == 0);
+    memcpy((void *) zmq_msg_data (&reply), "ERROR", 5);
+    rc = zmq_send(responder, &reply, 0);
+    assert(rc == 0);
+    rc = zmq_msg_close (&reply);      
+    assert(rc == 0);
   }
   zmq_term (context);
   return 0;
